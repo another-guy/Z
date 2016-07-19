0bb6a14c2ff0b22e3acb4363f61bfb50531557c6
Igor Soloydenko <soloydenko@gmail.com>
2016-07-18 18:23:42 -0700

    Add Trie implementation

---
+++ Z.Tests/Trees/TrieTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Z.Trees;
+
+namespace Z.Tests.Trees
+{
+    public class TrieTests
+    {
+        private readonly Trie sut = new Trie();
+
+        [Theory]
+        [MemberData(nameof(AddAndContainsWorkTogetherData))]
+        public void AddAndContainsWorkTogether(string[] words)
+        {
+            // Arrange
+            var knownWords = new List<string>();
+            foreach (var word in words)
+            {
+                // Assume
+                Assert.False(sut.Contains(word));
+                knownWords.Add(word);
+
+                // Act
+                sut.Add(word);
+
+                // Assert
+                foreach (var w in words)
+                    Assert.Equal(knownWords.Contains(w), sut.Contains(w));
+            }
+        }
+
+        public static IEnumerable<object[]> AddAndContainsWorkTogetherData =>
+            new List<object[]>
+            {
+                new object[] { new [] { "Alpha" } },
+                new object[] { new [] { "Alpha", "Beta" } },
+                new object[] { new [] { "Alpha", "Beta", "Astra" } }
+            };
+
+        [Theory]
+        [MemberData(nameof(AddAndIsPrefixWorkTogetherData))]
+        public void AddAndIsPrefixWorkTogether(string word, string[] nonPrefixes)
+        {
+            // Assume
+            for (var len = 1; len <= word.Length; len++)
+                Assert.False(sut.IsPrefix(word.Substring(0, len)));
+
+            // Act
+            sut.Add(word);
+
+            // Assert
+            Assert.True(sut.IsPrefix(string.Empty));
+
+            for (var len = 1; len <= word.Length; len++)
+                Assert.True(sut.IsPrefix(word.Substring(0, len)));
+
+            foreach (var nonPrefix in nonPrefixes)
+                Assert.False(sut.IsPrefix(nonPrefix));
+        }
+
+        public static IEnumerable<object[]> AddAndIsPrefixWorkTogetherData =>
+            new List<object[]>
+            {
+                new object[] { "Alpha", new [] { "X", "Y", "Z" } },
+                new object[] { "Beta", new [] { "A", "BB" } }
+            };
+    }
+}
---
+++ Z/Trees/Trie.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Z.Trees
+{
+    // TODO Consider making it a Collection and update tests then with Assert.Contains()
+    public sealed class Trie : TrieNode
+    {
+        private bool caseSensitive;
+
+        public Trie(bool caseSensitive = false)
+        {
+            // TODO Whitespaces check on both ends
+            this.caseSensitive = caseSensitive;
+        }
+
+        public void Add(string word)
+        {
+            if (caseSensitive == false)
+                word = word.ToLower();
+
+            base.Add(word);
+        }
+
+        public bool Contains(string word)
+        {
+            if (caseSensitive == false)
+                word = word.ToLower();
+
+            return base.Contains(word);
+        }
+
+        public bool IsPrefix(string prefix)
+        {
+            if (caseSensitive == false)
+                prefix = prefix.ToLower();
+
+            return base.IsPrefix(prefix);
+        }
+    }
+
+    public class TrieNode
+    {
+        private readonly IDictionary<char, TrieNode> inner = new Dictionary<char, TrieNode>();
+        private bool isWord = false;
+
+        internal TrieNode() { }
+        
+        protected void Add(string word, int charIndex = 0)
+        {
+            if (charIndex >= word.Length)
+            {
+                isWord = true;
+                return;
+            }
+
+            var chr = word[charIndex];
+
+            TrieNode suffix;
+            if (inner.TryGetValue(chr, out suffix) == false)
+            {
+                suffix = new TrieNode();
+                inner[chr] = suffix;
+            }
+            
+            suffix.Add(word, charIndex + 1);
+        }
+
+        protected bool Contains(string word, int charIndex = 0)
+        {
+            if (charIndex >= word.Length)
+            {
+                return isWord;
+            }
+
+            var chr = word[charIndex];
+
+            TrieNode suffix;
+            if (inner.TryGetValue(chr, out suffix) == false)
+            {
+                return false;
+            }
+
+            return suffix.Contains(word, charIndex + 1);
+        }
+
+        protected bool IsPrefix(string prefix, int charIndex = 0)
+        {
+            if (charIndex >= prefix.Length)
+            {
+                return true;
+            }
+
+            var chr = prefix[charIndex];
+
+            TrieNode suffix;
+            if (inner.TryGetValue(chr, out suffix) == false)
+            {
+                return false;
+            }
+
+            return suffix.IsPrefix(prefix, charIndex + 1);
+        }
+    }
+}
